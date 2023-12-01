fa848ca45e32e537bce5455d26d5828a06c24d2d
mirasac <50930731+mirasac@users.noreply.github.com>
2023-12-01 13:34:34 +0100

    Add Exercise_3

--- Exercise_2.py
+++ Exercise_2.py
@@ -8,9 +8,9 @@ def g_1(v):
     '''First derivative of g.'''
     return 2.0 * v
 
-def F_1(v, v_p, Sigma_p, u, g, Sigma_u, g_1):
+def F_1(v):
     '''First derivative of free energy.'''
-    return - (v - v_p) / Sigma_p + (u - g) / Sigma_u * g_1
+    return - (v - ex1.v_p) / ex1.Sigma_p + (ex1.u - ex1.g(v)) / ex1.Sigma_u * g_1(v)
 
 # Time simulation data.
 Delta_t = 0.01
@@ -20,20 +20,20 @@ t_max = 5.0
 def main():
     # Auxiliary data.
     n_t = int((t_max - t_min) / Delta_t) + 1
-    np_t = np.empty(n_t)
-    np_phi = np.empty(n_t)
+    t = np.empty(n_t)
+    phi = np.empty(n_t)
 
     # Run gradient ascent method.
-    phi = ex1.v_p
-    for i in range(0, n_t):
-        phi = phi + F_1(phi, ex1.v_p, ex1.Sigma_p, ex1.u, ex1.g(phi), ex1.Sigma_u, g_1(phi)) * Delta_t
-        np_t[i] = i
-        np_phi[i] = phi
-    print(f"phi = {phi}")
+    t[0] = 0.0
+    phi[0] = ex1.v_p
+    for i in range(1, n_t):
+        t[i] = i * Delta_t
+        phi[i] = phi[i - 1] + F_1(phi[i - 1]) * Delta_t
+    print(f"phi = {phi[-1]}")
 
     # Plot time evolution.
     matplotlib.use('Qt5Agg')
-    plt.plot(np_t, np_phi)
+    plt.plot(t, phi)
     plt.show()
 
 if __name__ == "__main__":
---
+++ Exercise_3.py
@@ -0,0 +1,47 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import Exercise_1 as ex1
+import Exercise_2 as ex2
+
+def phi_1(phi, epsilon_p, epsilon_u):
+    return epsilon_u * ex2.g_1(phi) - epsilon_p
+
+def epsilon_p_1(phi, epsilon_p):
+    return phi - ex1.v_p - ex1.Sigma_p * epsilon_p
+
+def epsilon_u_1(phi, epsilon_u):
+    return ex1.u - ex1.g(phi) - ex1.Sigma_u * epsilon_u
+
+def main():
+    # Auxiliary data.
+    n_t = int((ex2.t_max - ex2.t_min) / ex2.Delta_t) + 1
+    t = np.empty(n_t)
+    phi = np.empty(n_t)
+    epsilon_p = np.empty(n_t)
+    epsilon_u = np.empty(n_t)
+
+    # Simulate neural model.
+    t[0] = 0.0
+    phi[0] = ex1.v_p
+    epsilon_p[0] = 0.0
+    epsilon_u[0] = 0.0
+    for i in range(1, n_t):
+        t[i] = i * ex2.Delta_t
+        phi[i] = phi[i - 1] + phi_1(phi[i - 1], epsilon_p[i - 1], epsilon_u[i - 1]) * ex2.Delta_t
+        epsilon_p[i] = epsilon_p[i - 1] + epsilon_p_1(phi[i - 1], epsilon_p[i - 1]) * ex2.Delta_t
+        epsilon_u[i] = epsilon_u[i - 1] + epsilon_u_1(phi[i - 1], epsilon_u[i - 1]) * ex2.Delta_t
+    print(f"phi = {phi[-1]}")
+    print(f"epsilon_p = {epsilon_p[-1]}")
+    print(f"epsilon_u = {epsilon_u[-1]}")
+
+    # Plot time evolution.
+    matplotlib.use('Qt5Agg')
+    plt.plot(t, phi)
+    plt.plot(t, epsilon_p)
+    plt.plot(t, epsilon_u)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
